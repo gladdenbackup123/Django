fba104317d1273d9b66c5eeb51db4bda4fc5b820
Gladden <gladdenbackup@gmail.com>
2026-02-16 13:12:44 +0530

    Added Reset password

--- student_portal/students/urls.py
+++ student_portal/students/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('',views.home),
@@ -10,5 +11,10 @@ urlpatterns = [
     path('login/',views.login_user),
     path('logout/',views.logout_user),
     path('register',views.register_user),
-    path('change_password/',views.change_password)
+    path('change_password/',views.change_password),
+    
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'), name='password_reset'),
+    path('reset_password_done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
 ]
